f9c5e2368e6f2da17e8a63ced08229b4c6b5b176
jasmine Magdi <jesojolay@yahoo.com>
2017-09-18 21:59:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -68,12 +68,12 @@ def winner(board)
     wins << won?(board)[0]
     wins << won?(board)[1]
     wins << won?(board)[2]
-    puts board[wins[0]]
+    
     if board[wins[0]] == "X"
         win_X = "X"
         return win_X
     # elsif board[wins[0]] = "O"
-    
+
     end
     # if win_X != " "
     #     return win_X
