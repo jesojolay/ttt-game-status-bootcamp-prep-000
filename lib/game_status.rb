3b3999f27acb081025207929402242755aacd6cd
jasmine Magdi <jesojolay@yahoo.com>
2017-09-18 19:40:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,7 +56,7 @@ def draw?(board)
 end
 
 def over?(board)
-    if won?(board)  || draw?(board) || full?(board)
+    if won?(board)  || draw?(board) || full?(board) == false
         return true
     elsif full?(board) == false
         return false
