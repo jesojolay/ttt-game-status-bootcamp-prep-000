1fabf056a5f198ccd2ff4c3c0f8c653c98850ebb
jasmine Magdi <jesojolay@yahoo.com>
2017-09-18 22:03:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,4 @@
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = ["O", "X", "X", " ", " ", " ", " ", " ", " "]
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
