d9bbaab7b7f67c6b339df0b9f80f7ff873bda89f
jasmine Magdi <jesojolay@yahoo.com>
2017-09-18 20:02:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,7 +64,7 @@ def over?(board)
 end
 
 def winner(board)
-    wins = won?(board)
+    return wins = won?(board)
     win_X = board[wins[0]] = "X"
     win_O = board[wins[0]] = "O"
     if win_X == " "
